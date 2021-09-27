6625e6246cb113f645e202c00536adef46ff064c
ZerounNet <zerounnet@github.com>
2021-09-27 16:19:10 +0800

    Modified   run.py Modified   strategies.py

--- strategies.py
+++ strategies.py
@@ -1,12 +1,16 @@
 import numpy as np
+from abc import abstractmethod
 from wtpy.WtBtEngine import EngineType
+from wtpy.WtDataDefs import WtKlineData
 from wtpy.StrategyDefs import BaseCtaStrategy, CtaContext, BaseHftStrategy, HftContext
 
 class StateTransfer():
+    @abstractmethod
     @staticmethod
     def EngineType():
         raise NotImplementedError
 
+    @abstractmethod
     @staticmethod
     def Name():
         raise NotImplementedError
@@ -19,9 +23,9 @@ class StateTransfer():
     def TrainEndTime():
         return 202109091600
 
-    def __init__(self) -> None:
+    def __init__(self):
         self.set_action(0)
-        self.set_state(1, 1, False, {})
+        self.set_state(None, None, False, {})
         print('StateTransfer')
 
     def get_action(self):
@@ -31,14 +35,28 @@ class StateTransfer():
         self.__action__ = action
 
     def get_state(self):
-        done = True if np.random.randint(1, 100)==99 else False #是否结束
-        return self.__obs__, self.__reward__, done, self.__info__
+        return self.__obs__, self.__reward__, self.__done__, self.__info__
 
     def set_state(self, obs, reward, done, info):
         self.__obs__ = obs
         self.__reward__ = reward
         self.__done__ = done
         self.__info__ = info
+
+    @abstractmethod
+    @staticmethod
+    def calculate_obs(self, bars:WtKlineData):
+        raise NotImplementedError
+
+    @abstractmethod
+    @staticmethod
+    def calculate_reward(self, curr:int, best:int, worst:int):
+        raise NotImplementedError
+
+    @abstractmethod
+    @staticmethod
+    def calculate_done(self, obs, done):
+        raise NotImplementedError
 
 class TrainCTA(BaseCtaStrategy, StateTransfer):
     @staticmethod
@@ -59,12 +77,19 @@ class TrainCTA(BaseCtaStrategy, StateTransfer):
             )
     
     def on_calculate(self, context: CtaContext):
-        bars = context.stra_get_bars(
-            stdCode='DCE.c.HOT',
-            period='m1',
-            count=60
+        obs = self.calculate_obs(
+            context.stra_get_bars(
+                stdCode='DCE.c.HOT',
+                period='m1',
+                count=60
+                ))
+        reward = self.calculate_reward(
+            curr=context.stra_get_detail_profit(stdCode='DCE.c.HOT', usertag='', flag=0),
+            best=context.stra_get_detail_profit(stdCode='DCE.c.HOT', usertag='', flag=1),
+            worst=context.stra_get_detail_profit(stdCode='DCE.c.HOT', usertag='', flag=-1),
             )
-        # print(bars.closes)
+        done = self.calculate_done(obs, reward)
+        self.set_state(obs, reward, done, {})
 
 class TrainHFT(BaseHftStrategy, StateTransfer):
     @staticmethod
@@ -75,6 +100,16 @@ class DemoCTA(TrainCTA):
     @staticmethod
     def Name():
         return __class__.__name__
+
+    def calculate_obs(self, bars:WtKlineData):
+        return bars.closes
+
+    def calculate_reward(self, curr:int, best:int, worst:int):
+        return 1
+
+    def calculate_done(self, obs, done):
+        return True if np.random.randint(1, 100)==99 else False #是否结束
+
 
 class DemoHFT(TrainHFT):
     @staticmethod

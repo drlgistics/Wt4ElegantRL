eaa8f31a01422dbd57f3d661efcec8736ca569a4
ZerounNet <zerounnet@github.com>
2021-10-11 18:38:50 +0800

    Modified   run.py Modified   wtpy/CodeHelper.py Modified   wtpy/ContractMgr.py Modified   wtpy/CtaContext.py Modified   wtpy/ExtModuleDefs.py Modified   wtpy/ExtToolDefs.py Modified   wtpy/HftContext.py Modified   wtpy/ProductMgr.py Modified   wtpy/SelContext.py Modified   wtpy/SessionMgr.py Modified   wtpy/StrategyDefs.py Modified   wtpy/WtBtEngine.py Modified   wtpy/WtCoreDefs.py Modified   wtpy/WtDataDefs.py Modified   wtpy/WtDtEngine.py Modified   wtpy/WtDtServo.py Modified   wtpy/WtEngine.py Modified   wtpy/WtMsgQue.py Modified   wtpy/WtUtilDefs.py Modified   wtpy/__init__.py Modified   wtpy/apps/WtBtAnalyst.py Modified   wtpy/apps/WtCtaOptimizer.py Modified   wtpy/apps/WtHotPicker.py Modified   wtpy/apps/__init__.py Modified   wtpy/apps/datahelper/DHBaostock.py Modified   wtpy/apps/datahelper/DHDefs.py Modified   wtpy/apps/datahelper/DHFactory.py Modified   wtpy/apps/datahelper/DHRqData.py Modified   wtpy/apps/datahelper/DHTushare.py Modified   wtpy/apps/datahelper/__init__.py Modified   wtpy/apps/datahelper/db/MysqlHelper.py Modified   wtpy/apps/datahelper/db/initdb_mysql.sql Modified   wtpy/monitor/DataMgr.py Modified   wtpy/monitor/EventReceiver.py Modified   wtpy/monitor/PushSvr.py Modified   wtpy/monitor/WatchDog.py Modified   wtpy/monitor/WtBtMon.py Modified   wtpy/monitor/WtLogger.py Modified   wtpy/monitor/WtMonSvr.py Modified   wtpy/monitor/__init__.py Modified   wtpy/wrapper/ContractLoader.py Modified   wtpy/wrapper/PlatformHelper.py Modified   wtpy/wrapper/WtDtHelper.py Modified   wtpy/wrapper/WtDtServoApi.py Modified   wtpy/wrapper/WtDtWrapper.py Modified   wtpy/wrapper/WtExecApi.py Modified   wtpy/wrapper/WtMQWrapper.py Modified   wtpy/wrapper/__init__.py Modified   wtpy/wrapper/x64/WtBtPorter.dll Modified   wtpy/wrapper/x64/WtDataReader.dll Modified   wtpy/wrapper/x64/WtDataWriter.dll Modified   wtpy/wrapper/x64/WtDtHelper.dll Modified   wtpy/wrapper/x64/WtDtPorter.dll Modified   wtpy/wrapper/x64/WtDtServo.dll Modified   wtpy/wrapper/x64/WtExecMon.dll Modified   wtpy/wrapper/x64/WtMsgQue.dll Modified   wtpy/wrapper/x64/WtPorter.dll Modified   wtpy/wrapper/x64/WtRiskMonFact.dll Modified   wtpy/wrapper/x64/executer/WtExeFact.dll Modified   wtpy/wrapper/x64/parsers/ParserUDP.dll Modified   wtpy/wrapper/x64/traders/TraderCTP.dll Modified   wtpy/wrapper/x64/traders/TraderCTPMini.dll Modified   wtpy/wrapper/x64/traders/TraderCTPOpt.dll Modified   wtpy/wrapper/x64/traders/TraderFemas.dll Modified   wtpy/wrapper/x64/traders/TraderMocker.dll Modified   wtpy/wrapper/x64/traders/TraderXTP.dll Modified   wtpy/wrapper/x64/traders/TraderiTap.dll Modified   wtpy/wrapper/x86/WtBtPorter.dll Modified   wtpy/wrapper/x86/WtDtHelper.dll Modified   wtpy/wrapper/x86/WtDtPorter.dll Modified   wtpy/wrapper/x86/WtDtServo.dll Modified   wtpy/wrapper/x86/WtExecMon.dll Modified   wtpy/wrapper/x86/WtPorter.dll

--- run.py
+++ run.py
@@ -24,14 +24,14 @@ def debug():
         time_end=201912011500
         )
  
-    for i in range(10): #模拟训练10次
+    for i in range(1000): #模拟训练10次
         obs = env.reset()
         done = False
         action = 0
         while not done:
             action += 1 #模拟智能体产生动作
             obs, reward, done, info = env.step(action)
-            print('obs%s'%obs, 'reward%s'%reward, done, info)
+            # print('obs%s'%obs, 'reward%s'%reward, done, info)
         print('第%s次训练完成'%i)
     env.close()
 

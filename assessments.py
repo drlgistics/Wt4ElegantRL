c1319c4cfa1dd9fd5ad2e04f0bde82ce5ee2f877
zerounnet <zerounnet@github.com>
2021-10-16 14:52:16 +0800

    对reward进行数据后处理

--- assessments.py
+++ assessments.py
@@ -28,23 +28,35 @@ class Assessment():
     def done(self) -> float:
         raise NotImplementedError
 
+    @property
+    @abstractmethod
+    def assets(self) -> float:
+        raise NotImplementedError
 
-class SimpleAssessment(Assessment):
+
+class SimpleAssessment(Assessment): # 借鉴了neofinrl
+    gamma = 0.99 
+    scaling = 0.0005
     def reset(self):
-        self.__data__: list = [0]
+        self.__assets__: list = [0]
         self.__reward__: list = []
         self.__done__: bool = False
 
     def calculate(self, context: CtaContext):
         if self.__done__:
             return 
-        self.__data__.append(context.stra_get_fund_data(0)) #账户实时的动态权益
-        self.__reward__.append((self.__data__[-1]-self.__data__[-2])) # 以动态权益差分设计reward
+        self.__assets__.append(context.stra_get_fund_data(0)) #账户实时的动态权益
+        self.__reward__.append((self.__assets__[-1]-self.__assets__[-2])*self.scaling) # 以动态权益差分设计reward
 
         self.__done__ = False # 此处可以根据控制任务结束状态
 
     def finish(self):
-        self.__reward__.append(sum(self.__reward__)) # 在结束的时候把过程奖励做处理，作为整个训练的奖励
+        if self.__done__:
+            return 
+        gamma = 0
+        for reward in self.__reward__:
+            gamma = gamma*self.gamma + reward
+        self.__reward__.append(gamma) # 在结束的时候把过程奖励做处理，作为整个训练的奖励
         self.__done__ = True
 
     @property
@@ -54,3 +66,7 @@ class SimpleAssessment(Assessment):
     @property
     def done(self) -> float:
         return self.__done__
+
+    @property
+    def assets(self) -> float:
+        return self.__assets__[-1]

95988223c994be9adea54899754c2ab4e6af28fe
ZerounNet <zerounnet@github.com>
2021-10-26 18:24:47 +0800

    Added      analysts.py

---
+++ analysts.py
@@ -0,0 +1,16 @@
+from wtpy.apps import WtBtAnalyst
+from glob import glob
+from tqdm.auto import tqdm
+from os.path import dirname
+
+for i in tqdm(glob('./outputs_bt/*/funds.csv')):
+    folder = dirname(i)
+    name = folder[13:]
+    analyst = WtBtAnalyst()
+    folder = "./outputs_bt/%s/" % name
+    analyst.add_strategy(
+        name, folder=folder, init_capital=500000, rf=0.04, annual_trading_days=240)
+    try:
+        analyst.run_new('%s/%s_PnLAnalyzing.xlsx' % (folder, name))
+    except:
+        analyst.run('%s/%s_PnLAnalyzing.xlsx' % (folder, name))
